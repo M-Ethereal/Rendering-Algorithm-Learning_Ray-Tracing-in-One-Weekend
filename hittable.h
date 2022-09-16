42170bc4737b4bf0797925b67ae6c3839120de99
Ethereal <ziyi.zhou.gr@dartmouth.edu>
2022-09-16 16:24:14 -0400

    add abstract class hittable and sphere

---
+++ hittable.h
@@ -0,0 +1,17 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record {
+  point3 p;
+  vec3 normal;
+  double t;
+};
+
+class hittable {
+  public:
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif
