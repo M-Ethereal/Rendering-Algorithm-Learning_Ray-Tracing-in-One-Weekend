ee389d3eed99b6d4b86ee852d3c4d0a1222a72b6
Ethereal <ziyi.zhou.gr@dartmouth.edu>
2022-09-16 17:48:33 -0400

    add some new headers

--- hittable.h
+++ hittable.h
@@ -7,6 +7,14 @@ struct hit_record {
   point3 p;
   vec3 normal;
   double t;
+  //add a new contribute to record where the normal towards
+  bool front_face;
+
+  //we want the normal always point against the ray
+  inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
 };
 
 class hittable {
--- sphere.h
+++ sphere.h
@@ -38,7 +38,9 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
 
   rec.t = root;
   rec.p = r.at(rec.t);
-  rec.normal = (rec.p - center) / radius;
+  // rec.normal = (rec.p - center) / radius;
+  vec3 outward_normal = (rec.p - center) / radius;
+  rec.set_face_normal(r, outward_normal);
 
   return true;
 }
